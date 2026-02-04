8fb23da302a3acd3ad85d836c0e6db3dfda0f032
Benjamin Buhler <2802675+bbuhler@users.noreply.github.com>
2026-02-04 12:21:45 +0000

    style: formatting

--- src/routes/(app)/orga/calendar/lunar/+page.server.ts
+++ src/routes/(app)/orga/calendar/lunar/+page.server.ts
@@ -74,7 +74,7 @@ function getMoonPhases(refDate = new Date()) {
 	return {
 		lastNewMoon: { date: lastNewMoonDate, emoji: phaseEmojis.newMoon },
 		nextNewMoon: { date: nextNewMoonDate, emoji: phaseEmojis.newMoon },
-		fullMoon: cyclePhases.find(phase => phase.phase === 'fullMoon'),
+		fullMoon: cyclePhases.find((phase) => phase.phase === 'fullMoon'),
 		cyclePhases
 	};
 }
@@ -89,30 +89,36 @@ function byEvents(item: CalendarComponent) {
 	return item.type === 'VEVENT';
 }
 
-async function eventsByView(start: Date, end: Date, icalUrl: string, filterPrivate = false): Promise<Event[]> {
+async function eventsByView(
+	start: Date,
+	end: Date,
+	icalUrl: string,
+	filterPrivate = false
+): Promise<Event[]> {
 	const calendarResponse = await ical.async.fromURL(icalUrl);
 	const icalEvents: VEvent[] = Object.values(calendarResponse).filter(byEvents);
 
 	return icalEvents
-		.filter(event => {
-			const between = event.start.getTime() >= start.getTime() && event.start.getTime() <= end.getTime();
+		.filter((event) => {
+			const between =
+				event.start.getTime() >= start.getTime() && event.start.getTime() <= end.getTime();
 			const includePrivate = !filterPrivate || event.class !== 'PRIVATE';
 			return between && includePrivate;
 		})
 		.reduce((events, event) => {
 			if (event.rrule) {
-				event.rrule.between(start, end).forEach(date => {
+				event.rrule.between(start, end).forEach((date) => {
 					events.push({
 						title: event.summary,
 						start: date,
-						end: new Date(date.getTime() + event.end.getTime() - event.start.getTime()),
+						end: new Date(date.getTime() + event.end.getTime() - event.start.getTime())
 					});
 				});
 			} else {
 				events.push({
 					title: event.summary,
 					start: event.start,
-					end: event.end,
+					end: event.end
 				});
 			}
 			return events;
@@ -123,9 +129,22 @@ export const load: PageServerLoad = async ({ url }) => {
 	const refDate = url.searchParams.get('refDate');
 	const moonPhases = getMoonPhases(refDate ? new Date(refDate) : new Date());
 
-	const events = await eventsByView(moonPhases.lastNewMoon.date, moonPhases.nextNewMoon.date, EVENTS_ICAL);
-	const retreats = await eventsByView(moonPhases.lastNewMoon.date, moonPhases.nextNewMoon.date, RETREATS_ICAL);
-	const community = await eventsByView(moonPhases.lastNewMoon.date, moonPhases.nextNewMoon.date, COMMUNITY_ICAL, true);
+	const events = await eventsByView(
+		moonPhases.lastNewMoon.date,
+		moonPhases.nextNewMoon.date,
+		EVENTS_ICAL
+	);
+	const retreats = await eventsByView(
+		moonPhases.lastNewMoon.date,
+		moonPhases.nextNewMoon.date,
+		RETREATS_ICAL
+	);
+	const community = await eventsByView(
+		moonPhases.lastNewMoon.date,
+		moonPhases.nextNewMoon.date,
+		COMMUNITY_ICAL,
+		true
+	);
 
 	return { ...moonPhases, events, retreats, community };
 };
--- src/routes/api/instagram/webhook/+server.ts
+++ src/routes/api/instagram/webhook/+server.ts
@@ -3,26 +3,29 @@ import type { RequestHandler } from './$types';
 import { INSTAGRAM_TOKEN } from '$env/static/private';
 
 interface InstagramWebhookResponse {
-    object: string;
-    entry: object[];
-    id: string;
-    changed_fields: string[];
-    changes: object[];
-    time: number;
+	object: string;
+	entry: object[];
+	id: string;
+	changed_fields: string[];
+	changes: object[];
+	time: number;
 }
 
 export const GET: RequestHandler = ({ url }) => {
-  if (url.searchParams.get('hub.mode') === 'subscribe' && url.searchParams.get('hub.verify_token') === INSTAGRAM_TOKEN) {
-	return new Response(url.searchParams.get('hub.challenge'));
-  } 
-  
-  error(400);
+	if (
+		url.searchParams.get('hub.mode') === 'subscribe' &&
+		url.searchParams.get('hub.verify_token') === INSTAGRAM_TOKEN
+	) {
+		return new Response(url.searchParams.get('hub.challenge'));
+	}
+
+	error(400);
 };
 
 export const POST: RequestHandler = async ({ request }) => {
-    const body: InstagramWebhookResponse = await request.json();
+	const body: InstagramWebhookResponse = await request.json();
 
-    console.log(body);
-    
-    return new Response(null, { status: 202 });
+	console.log(body);
+
+	return new Response(null, { status: 202 });
 };
